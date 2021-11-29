90bf2e8b1b67c60d4a4bfde1d8d32f693384c0a2
valarcon29011995 <92767134+valarcon29011995@users.noreply.github.com>
2021-11-29 12:55:07 -0500

    Add files via upload

---
+++ Caso1/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caso1
+{
+    internal class Pedido
+    {
+        public int ID;
+        public int Prioridad;
+        public int Costo;
+        public string pedido;
+        public string Cliente;
+
+        public Pedido(int ID, int Prioridad, int Costo, string pedido, string Cliente)
+        {
+            this.ID = ID;
+            this.Prioridad = Prioridad;
+            this.Costo = Costo;
+            this.pedido = pedido;
+            this.Cliente = Cliente;
+        }
+    }
+}
---
+++ Caso1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caso1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Creando los pedidos.....\n");
+            Pedido p1 = new Pedido(001, 3, 12, "Pollo Saltado", "Miriam");
+            Pedido p2 = new Pedido(002, 2, 13, "Chaufa", "Victor");
+            Pedido p3 = new Pedido(003, 1, 14, "Makis", "Ricardo");
+            Pedido p4 = new Pedido(004, 4, 15, "Ají de Gallina", "Manuel");
+            Pedido p5 = new Pedido(005, 5, 16, "Pollo Frito", "Pamela");
+            ArbolBinario arbolbin = new ArbolBinario();
+            Console.Write("Ingresando los pedidos al sistema.....\n");
+            arbolbin.InsertarNodo(p1);
+            arbolbin.InsertarNodo(p2);
+            arbolbin.InsertarNodo(p3);
+            arbolbin.InsertarNodo(p4);
+            arbolbin.InsertarNodo(p5);
+
+            Console.WriteLine("Listando los pedidos en preorden....");
+            arbolbin.Preorden(arbolbin.GetRaiz());
+            Console.WriteLine("");
+            Console.WriteLine("Listando los pedidos en inorden....");
+            arbolbin.Inorden(arbolbin.GetRaiz());
+            Console.WriteLine("");
+            Console.WriteLine("Listando los pedidos en postorden....");
+            arbolbin.Postorden(arbolbin.GetRaiz());
+            Console.WriteLine("");
+
+            Console.WriteLine("***********AGREGANDO NUEVO PEDIDO**********");
+            Pedido p6 = new Pedido(006, 0, 35, "Estofado de carne", "Heidi");
+            arbolbin.InsertarNodo(p6);
+            Console.WriteLine("************************************************");
+
+            Console.WriteLine("Listando los pedidos en postorden....");
+            arbolbin.Postorden(arbolbin.GetRaiz());
+            Console.WriteLine("");
+            Console.WriteLine("Listando los pedidos en inorden....");
+            arbolbin.Inorden(arbolbin.GetRaiz());
+            Console.WriteLine("");
+            Console.WriteLine("Listando los pedidos en preorden....");
+            arbolbin.Preorden(arbolbin.GetRaiz());
+            Console.WriteLine("");
+            Console.ReadLine();
+
+        }
+    }
+}
